ba6479ecba03596cb856a7453938f3fdfd7317e1
OMahfouz13 <omarmahfouzpro@gmail.com>
2025-10-27 16:39:13 -0400

    feat: created FacultyMember class

---
+++ FacultyMember.java
@@ -0,0 +1,10 @@
+public class FacultyMember {
+    String name;
+    double salary;
+
+    public FacultyMember(String name, double salary){
+        name = this.name;
+        salary = this.salary;
+    }
+    
+}
