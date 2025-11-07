070a0c672f476fc6eedb89981a0c9509ffb64170
tohackacat <203383101+tohackacat@users.noreply.github.com>
2025-11-07 15:21:51 -0500

    feat: implemented api by contract with placeholders values

--- Student.java
+++ Student.java
@@ -1,1 +1,17 @@
-public class Student {}
+public class Student {
+  public String getId() {
+    return "MK001"; // TODO: return real id
+  }
+
+  public String getName() {
+    return "Omar"; // TODO: return real Name
+  }
+
+  public double getGpa() {
+    return 3.5; // TODO: return real gpa
+  }
+
+  public int getCredReqs() {
+    return 60; // TODO: return real credits requirements
+  }
+}
