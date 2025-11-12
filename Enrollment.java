8af265a4b07745246e8af32defbe761df7a20c2b
OMahfouz13 <omarmahfouzpro@gmail.com>
2025-11-12 17:54:03 -0500

    Changed Enrollment to a record from a class

--- Enrollment.java
+++ Enrollment.java
@@ -1,26 +1,7 @@
 /*
- * An Enrollment class that holds a course, an instructor,
- * and a dynamically resizeable ArrayList of students.
- * It contains a method to add a student and set a new instructor.
+ * An Enrollment record that holds a courseID, a studentID.
  */
 
-public record Enrollment() {
-} Enrollment {
-    //Attributes
-    private String courseId;
-    private String studentId; 
-    
-    //Constructor
-    public Enrollment(String courseId, String studentId){
-        this.courseId = courseId;
-        this.studentId = studentId;
-    }
-
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("Course ID: ").append(courseId)
-          .append("\nStudent ID:").append(studentId);
-
-        return sb.toString();
-    }
+public record Enrollment(String courseId, String studentId) {
 }
+
