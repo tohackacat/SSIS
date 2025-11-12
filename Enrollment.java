142857c4c68452c78e70baef9cc4674a2eed0b45
OMahfouz13 <omarmahfouzpro@gmail.com>
2025-11-12 17:25:30 -0500

    Changed Enrollment class to hold only courseId and studentId

--- Enrollment.java
+++ Enrollment.java
@@ -3,53 +3,22 @@
  * and a dynamically resizeable ArrayList of students.
  * It contains a method to add a student and set a new instructor.
  */
-import java.util.ArrayList;
 
 public class Enrollment {
     //Attributes
-    private Faculty instructor;
-    private Course course;
-    private ArrayList<Student> students; 
+    private String courseId;
+    private String studentId; 
     
     //Constructor
-    public Enrollment(Faculty instructor, Course course){
-        this.instructor = instructor;
-        this.course = course;
-    }
-
-    //Getters
-    public Faculty getFaculty(){
-        return instructor;
-    }
-
-    public Course getCourse(){
-        return course;
-    }
-
-    public ArrayList<Student> getStudents(){
-        return students;
-    }
-
-    //Add Students
-    public void addStudent(Student student){
-        students.add(student); 
-    }
-
-    //Set a new Instructor
-    public void setFaculty(Faculty instructor){
-        this.instructor = instructor;
+    public Enrollment(String courseId, String studentId){
+        this.courseId = courseId;
+        this.studentId = studentId;
     }
 
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("Course: ").append(course.getName())
-          .append("\nCourse ID: ").append(course.getId())
-          .append("\nInstructor: ").append(instructor.getName())
-          .append("\n Students: ");
-        
-        for(Student s: students){
-            sb.append(s.getName());
-        }
+        sb.append("Course ID: ").append(courseId)
+          .append("\nStudent ID:").append(studentId);
 
         return sb.toString();
     }
