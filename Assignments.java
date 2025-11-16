eacdbed3f48786e596172b2c262643ceb7391078
OMahfouz13 <omarmahfouzpro@gmail.com>
2025-11-15 21:23:48 -0500

    created the assignments abstract class

---
+++ Assignments.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+public abstract class Assignments {
+    ArrayList<Enrollment> enrollments;
+    Instructing instruct;
+    LocalDateTime dueDateTime;
+    double grade;
+    double gradeWeight;
+    
+}
