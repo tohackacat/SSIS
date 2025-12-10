3725078fea7434430bb5f8c76db48540a82390b3
mila1598 <cef159806@gmail.com>
2025-12-10 11:14:43 -0500

    Implementation of Teaching DTO

---
+++ src/main/java/org/example/teaching/dto/TeachingDto.java
@@ -0,0 +1,11 @@
+package org.example.teaching.dto;
+public record TeachingDto(
+        String id,
+        String courseId,
+        String courseCode,
+        String courseName,
+        String facultyId,
+        String facultyName,
+        String role
+) {
+}
