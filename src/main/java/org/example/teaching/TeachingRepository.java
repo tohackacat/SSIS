65e68940b6a9ec4f3b4e7b9a999e2578c74400c0
mila1598 <cef159806@gmail.com>
2025-12-10 13:35:39 -0500

    Succesfully created the TeachingRepository

---
+++ src/main/java/org/example/teaching/TeachingRepository.java
@@ -0,0 +1,16 @@
+package org.example.teaching;
+import java.util.Optional;
+
+import org.example.EntityId;
+
+public interface TeachingRepository {
+    Iterable<Teaching> findAll();
+
+    Optional<Teaching> findById(EntityId id);
+
+    void insert(Teaching teaching);
+    void update(Teaching teaching);
+    void delete(EntityId id);
+
+    
+}
