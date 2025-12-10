7057ffad020b3de87ebea59b81f04a16742ea179
OMahfouz13 <omarmahfouzpro@gmail.com>
2025-12-10 15:50:26 -0500

    Enrollment file and its repository interface

---
+++ src/main/java/org/example/enrollment/EnrollmentRepository.java
@@ -0,0 +1,17 @@
+package org.example.enrollment;
+
+import java.util.Optional;
+
+import org.example.core.EntityId;
+
+public interface EnrollmentRepository {
+    Iterable<Enrollment> findAll();
+    Optional<Enrollment> findbyId(EntityId id);
+
+    void insert(Enrollment enrollment);
+
+    void update(Enrollment enrollment);
+
+    void delete(Enrollment enrollment);
+    
+}
