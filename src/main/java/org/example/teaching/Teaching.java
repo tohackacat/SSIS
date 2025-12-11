0e483992f593805ace32c2e7ab4f98825a01df57
mila1598 <cef159806@gmail.com>
2025-12-11 12:13:16 -0500

    Updated again Teaching record by changing name file and fields

---
+++ src/main/java/org/example/teaching/Teaching.java
@@ -0,0 +1,8 @@
+package org.example.teaching;
+import org.example.core.EntityId;
+/*
+ * An Instructing recod that holds a courseId and a facultyId.
+ */
+public record Teaching(EntityId id,EntityId courseId, EntityId facultyId, String role) {
+
+}
