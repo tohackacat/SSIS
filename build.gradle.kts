f9515f1e78a0a4e7c7b5f64aa2206dad298b64b8
tohackacat <203383101+tohackacat@users.noreply.github.com>
2025-12-15 08:46:06 -0500

    feat: added proper dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,15 @@ java {
 repositories {
     mavenCentral()
 }
+
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.jline:jline:3.30.0")
+    implementation("org.jline:jline-terminal-jansi:3.30.6")
+    implementation("org.jline:jline-terminal-jni:3.30.6")
+    implementation("net.java.dev.jna:jna:5.18.1")
+    implementation("org.xerial:sqlite-jdbc:3.51.1.0")
+}
